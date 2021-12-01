3253b6ef00510f1aa8c773b156ee68e3677563d9
ashraf <ashrafawad2@gmail.com>
2021-12-01 13:08:11 +0200

    Add Pushing Message to Chat   - Add Login Authentication without Database   - Add Registeration Funcitinality (Not Last Version yet)

--- server/server.js
+++ server/server.js
@@ -2,26 +2,120 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketIo = require("socket.io");
-
-const publicPath = path.join(__dirname, "/../public");
-const port = process.env.PORT || 3000;
+const formatMessage = require("../utils/messages");
+const session = require("express-session");
+const flash = require("express-flash");
+const bodyParser = require("body-parser");
+const { v4: uuid } = require("uuid");
 
 // Create Server From Express App
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Use Static Path
+const publicPath = path.join(__dirname, "/../public");
 app.use(express.static(publicPath));
+
+const port = process.env.PORT || 3000;
+
+// Users Array
+const users = [
+  {
+    email: "ashraf@ashraf.com",
+    password: 123456,
+  },
+  {
+    email: "omar@omar.com",
+    password: 123456,
+  },
+  {
+    email: "amira@amira.com",
+    password: 123456,
+  },
+];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: uuid(),
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// app.use(flash());
+
+app.get("/", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/register", (req, res) => {
+  try {
+    users.push({
+      id: Data.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    res.redirect("/login");
+  } catch {
+    res.redirect("/register");
+    console.log("Catch Error");
+  }
+  console.log(users);
+});
+
+app.post("/login", (req, res) => {
+  let username = req.body.email;
+  let password = req.body.password;
+
+  if (
+    users.some((user) => {
+      return user.email == username && user.password == password;
+    })
+  ) {
+    console.log(username);
+    req.session.user = username;
+    res.redirect("/chat");
+  } else {
+    console.error("Login Error"); // return done(null, false, { message: "Email or Password Incorrect" });
+  }
+});
+
+app.get("/chat", (req, res) => {
+  res.render("index.ejs", { username: req.session.user });
+});
 
 // Make Server Listen to Client Events
 io.on("connection", (socket) => {
-  console.log("A new User just Connected");
+  console.log("message", formatMessage("User", "A new User just Connected"));
+
+  // Listen to chatMessage
+  socket.on("chatMessage", (data) => {
+    io.emit("message", {
+      message: data.message,
+      socketId: socket.id,
+    });
+  });
 
   socket.on("disconnect", (socket) => {
-    console.log("User was Disconnected!");
+    console.log("message", formatMessage("User", "User was Disconnected!"));
   });
 });
 
+// Server Listen to Port 3000
 server.listen(port, () => {
   console.log(`server is Up on port ${port}`);
 });
